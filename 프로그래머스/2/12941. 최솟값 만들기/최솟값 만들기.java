ff53f5050518a8d137a90ed9c0e4f3981a4820e3
Youjung <118320449+yj0111@users.noreply.github.com>
2024-07-01 13:27:30 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.33 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int len = A.length;
+        for(int i = 0; i<len; i++){
+            answer+= A[i] * B[len-i-1];
+        }
+
+        return answer;
+    }
+}
