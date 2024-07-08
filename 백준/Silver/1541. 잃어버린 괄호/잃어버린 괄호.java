c6b6b522d47ae2634ebf08680ea91bf040090fe0
Youjung <118320449+yj0111@users.noreply.github.com>
2024-07-08 15:04:00 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 176 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.next();
+        String[] str = input.split("[-]"); // - 이나 [-] 이나 똑같음!
+//        System.out.println(Arrays.toString(str));
+        int answer = 0;
+
+        for(int i = 0; i<str.length; i++){
+            String[] temp = str[i].split("[+]");
+//            System.out.println(Arrays.toString(temp));
+            if(i == 0){
+                answer += cal(temp);
+            }else {
+                answer -= cal(temp);
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static int cal(String[] s){
+       int sum = 0;
+       for(int i = 0; i<s.length; i++){
+           sum+= Integer.parseInt(s[i]);
+       }
+       return sum;
+    }
+}
