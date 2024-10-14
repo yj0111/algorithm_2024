978065d950f00db3f0e5b8607f426bd8e9526e51
Youjung <118320449+yj0111@users.noreply.github.com>
2024-10-14 20:56:10 +0900

    [Silver IV] Title: 카드2, Time: 224 ms, Memory: 48972 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 1; i<=N; i++){
+            q.offer(i);
+        }
+
+        while(q.size() != 1) {
+            q.poll(); //맨 위 버려
+            q.offer(q.poll());
+        }
+
+        System.out.println(q.poll());
+    }
+}
